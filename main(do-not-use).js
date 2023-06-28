66d187fe0974912f4e82e7c3e34ed2793a95476b
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-06-27 20:48:42 -0500

    Update main(do-not-use).js

--- main(do-not-use).js
+++ main(do-not-use).js
@@ -1,1 +1,31 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('1s 1o=n.B.H;n.B.H="<u d=\'V\'> <18 l=\'1j\'>1k 1n</18> </u> <A d=\'y\' l=\'y\'> <1l d=\'k\' l=\'k\' P=\'1p 1H 1E 1F\' 1G=\'i\'/> </A> <u d=\'R\'> <a 19=\'6://h.E/1C\' 14=\'1b\' 11=\'13\'> <v 1e=\'6://1h-1i-1g.1f.10/v/h.K\' d=\'h\' l=\'h\'/> </a> <a 19=\'6://f.1x/1w\' 14=\'1b\' 11=\'13\'> <v 1e=\'6://1h-1i-1g.1f.10/v/f.K\' d=\'f\' l=\'f\'/> </a> </u><M>B{T-p:1z;}.y{ 9:1y%; 4:7%; p: #o; g: c; G:26.5%; r:1v%; } .k{ i-m: e; T: 1D; I-Q: S; I-Q: S; I: 1m 1t #o; p: #o; 9:j%; 4:j%; } .V{ X-1r: \'1q\', 1u, 1B, 25-2g; g: c; 9: j%; 4: 2j; r:17%; 29: e; m: e; i-m: e; p: #o; } ::P { 2a: 2b; p: #o; X-2c: 1.2d; } .R{ m: e; i-m: e; g: c; 9: j%; 4: b; 2k: 0%; G: 0%; } .h{ g: c; 9:b; 4:b; L:U%; t:0.5; } .f{ g: c; 4:j%; 9:b; 4:1I; t:0.5; G:U%; r:8%; } .f:W{ t:1; } .h:W{ t:1; } .x{ g: c; 9:b; 4:b; r:N; L:N; }</M>";w 12(z=\'\'){C(/^D(s?):\\/\\//.2i(z)||z.2h(\'.\')&&z.2f(0,1)!==\' \')J 27;J 1P}1d A=n.1c("y");1d 2=n.1c("k");A.1Q("1R",1S(1a)=>{1a.1O();C(!12(2.3))2.3=\'6://1N.1J.E/k?q=\'+2.3;1K C(!(2.3.15(\'6://\')||2.3.15(\'D://\')))2.3=\'D://\'+2.3;1L(\'6://1M.1T.E/1U/21?22=\'+2.3).16(w(F){C(F.23!==24){Y(\'20 1Z a 1V 1W 1X 1Y.\');J}F.i().16(w(Z){n.B.H=Z})}).2e(w(O){Y(\'28 1A\',O)})});',62,145,'||address|value|height||https|||width||70px|absolute|class|center|discord|position|github|text|100|search|id|align|document|FFFFFF|color||top||opacity|div|img|function||searchbox|val|form|body|if|http|com|response|left|innerHTML|border|return|png|right|style|0px|err|placeholder|radius|icons|18px|background|40|title|hover|font|alert|data|app|rel|isUrl|noopener|target|startsWith|then||h1|href|event|_blank|getElementById|const|src|netlify|static5|dm|unbl0cker|title1|DM|input|2px|Unblocker|origincode|Search|Roboto|family|var|solid|Arial|50|hrXXUeWgrn|gg|47|black|Error|Helvetica|dragon731012|transparent|web|freely|type|the|58px|google|else|fetch|api|www|preventDefault|false|addEventListener|submit|async|codetabs|v1|problem|with|your|url|was|there|proxy|quest|status|200|sans||true|Fetch|float|margin|auto|size|2em|catch|substr|serif|includes|test|100px|bottom'.split('|'),0,{}))
+var origincode = document.body.innerHTML;
+
+document.body.innerHTML = "<div class='title'> <h1 id='title1'>DM Unblocker</h1> </div> <form class='searchbox' id='searchbox'> <input class='search' id='search' placeholder='Search the web freely' type='text'/> </form> <div class='icons'> <a href='https://github.com/dragon731012' target='_blank' rel='noopener'> <img src='https://dm-unbl0cker-static5.netlify.app/img/github.png' class='github' id='github'/> </a> <a href='https://discord.gg/hrXXUeWgrn' target='_blank' rel='noopener'> <img src='https://dm-unbl0cker-static5.netlify.app/img/discord.png' class='discord' id='discord'/> </a> </div><style>body{background-color:black;}.searchbox{ width:47%; height:7%; color: #FFFFFF; position: absolute; left:26.5%; top:50%; } .search{ text-align: center; background: transparent; border-radius: 18px; border-radius: 18px; border: 2px solid #FFFFFF; color: #FFFFFF; width:100%; height:100%; } .title{ font-family: 'Roboto', Arial, Helvetica, sans-serif; position: absolute; width: 100%; height: 100px; top:17%; float: center; align: center; text-align: center; color: #FFFFFF; } ::placeholder { margin: auto; color: #FFFFFF; font-size: 1.2em; } .icons{ align: center; text-align: center; position: absolute; width: 100%; height: 70px; bottom: 0%; left: 0%; } .github{ position: absolute; width:70px; height:70px; right:40%; opacity:0.5; } .discord{ position: absolute; height:100%; width:70px; height:58px; opacity:0.5; left:40%; top:8%; } .discord:hover{ opacity:1; } .github:hover{ opacity:1; } .x{ position: absolute; width:70px; height:70px; top:0px; right:0px; }</style>";
+
+function isUrl(val = '') {
+	if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
+	return false;
+}
+
+const form = document.getElementById("searchbox");
+const address = document.getElementById("search");
+form.addEventListener("submit", async(event) => {
+	event.preventDefault();
+	if (!isUrl(address.value)) address.value = 'https://www.google.com/search?q=' + address.value;
+	else if (!(address.value.startsWith('https://') || address.value.startsWith('http://'))) address.value = 'http://' + address.value;
+	fetch('https://api.codetabs.com/v1/proxy?quest=' + address.value)
+		.then(
+			function(response) {
+				if (response.status !== 200) {
+					alert('there was a problem with your url.');
+					return;
+				}
+				response.text().then(function(data) {
+					document.body.innerHTML = data;
+				});
+			}
+		)
+		.catch(function(err) {
+			alert('Fetch Error', err);
+		});
+});
